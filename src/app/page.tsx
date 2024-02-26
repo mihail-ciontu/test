51138406d1f8279902f5c604d7f0c6faf24d4b4a
Mihail <mihail.ciontu@saltandpepper.co>
2024-02-26 08:52:49 +0200

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "} me
+            By{" "} mes
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
